9bc72ec589e53caf827fcd6f31461448683f53e6
Kishan <kishansinghthakur27@gmail.com>
2024-09-21 16:03:46 +0530

    updated

--- 04_week-4/02_createing_https_server/01_offline_vedio/main.js
+++ 04_week-4/02_createing_https_server/01_offline_vedio/main.js
@@ -13,6 +13,18 @@ function sum( a , b){
 //     })
     
 // });
+/**
+ * create a route for the root URL
+ * 
+ * Query Parameters: It is a way to send data to the server as key-value pairs. It is appended to the URL after a question mark (?).
+ * Example: http://localhost:3000/?n=5
+ * Here n is the key and 5 is the value. n is query parameter and 5 is the value of n.
+ * Use req.query to access the query parameters in the request object.
+ * 
+ * If you want to add multiple query parameters, you can separate them with an ampersand (&).
+ * Example: http://localhost:3000/?a=5&b=10&c=15
+ * Here a, b, and c are query parameters and 5, 10, and 15 are their respective values.
+ */
 app.get("/sum/:a/:b" , function(req , res){
     let a = Number(req.params.a);
     let b = Number(req.params.b);
